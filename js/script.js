5c67d7455af3155f2a6405b74efc7583f7ab9a04
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-10 02:40:27 +0200

    [UPD] creato un ciclo while per generare 5 numeri random

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// GENERO 5 NUMERI RANDOM
+// dichiaro l'array che contiene i numeri random
+let randomNumbers = [];
+// imposto un ciclo while per determinare il numero di valori da generare
+while (randomNumbers.length < 5) {
+    // dichiaro una variabile che ha come valore un numero random che verrà ciclata 5 volte
+    let num = Math.floor(Math.random() * 159 + 1);
+    // imposto una condizione per verificare che non siano presenti numeri uguali all'interno dell'array
+    if (!randomNumbers.includes(num)) {
+        // pusho il numero generato all'interno dell'array
+        randomNumbers.push(num);
+    }
+}
+
 
 
 
