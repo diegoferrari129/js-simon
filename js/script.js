509a794dd32feff63ecc1198d807b3da3fec21be
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-10 15:31:26 +0200

    [UPD] aggiunta l'interazione con l'utente per aquisire dei numeri e aggiunto il calcolo dei numeri uguali a quelli generati random

--- js/script.js
+++ js/script.js
@@ -19,18 +19,33 @@ let displayNumbers = document.getElementById('random-numbers');
 displayNumbers.innerHTML = randomNumbers;
 console.log(randomNumbers);
 
-// IMPOSTO UN INTERVALLO PER LA VISUALIZZAZIONE DEI 5 NUMERI RANDOM
-let timer = setInterval(function() {
-    let userInput = [];
-    for (let i = 0; i < 5; i++ ) {
-        userInput.push(prompt('Digita i numeri precedentemente visualizzati'));
-    }
-}, 6000);
-
 // imposto un timeout per nascondere la visualizzazione dei numeri random dopo 5 secondi
 let hideNumbers = setTimeout(function () { 
     displayNumbers.innerHTML = '';
 }, 5000);
+
+// IMPOSTO UN INTERVALLO PER LA VISUALIZZAZIONE DEI 5 NUMERI RANDOM
+let timer = setTimeout(function() {
+    let userInput = [];
+    for (let i = 0; i < 5; i++ ) {
+        userInput.push(parseInt(prompt('Digita i numeri precedentemente visualizzati')));
+    }
+
+    let correctNumbers = [];
+
+    for (let i = 0; i < 5; i++ ) {
+        if (randomNumbers.includes(userInput[i])) {
+            correctNumbers.push(userInput[i]);
+        };
+    };
+
+    displayNumbers.innerHTML = `Hai indovinato ${correctNumbers.length} numeri: ${correctNumbers}`;
+
+}, 6000);
+
+
+
+
 
 
 
