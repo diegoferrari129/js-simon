012b5f23a50674092b339c86679f33587256844e
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-10 02:56:34 +0200

    [UPD] impostato un intervallo per visualizzare i numeri random nella pagina

--- js/script.js
+++ js/script.js
@@ -13,11 +13,33 @@ while (randomNumbers.length < 5) {
 }
 
 // MOSTRO I 5 NUMERI RANDOM NELLA PAGINA
-// recupero l'elemento dal dom
+// recupero l'elemento dom per mostrare il valore dell'array randomNumbers
 let displayNumbers = document.getElementById('random-numbers');
-// mostro il valore di randomNumbers nella pagina
+// mostro il valore dell'array randomNumbers nella pagina
 displayNumbers.innerText = randomNumbers;
 console.log(randomNumbers);
+
+// IMPOSTO UN INTERVALLO PER LA VISUALIZZAZIONE DEI 5 NUMERI RANDOM
+// dichiaro una variabile con il valore del timer
+let seconds = 5;
+// recupero l'elemento dal dom per mostrare il valore della variabile seconds
+let countdown = document.getElementById('countdown');
+// mostro nella pagina il timer
+countdown.innerText = seconds;
+// dichiaro un intervallo di 1 secondo
+let timer = setInterval(function() {
+    // mostro nella l'azione dell'intervallo
+    countdown.innerText = seconds;
+    // imposto una condizione
+    if (seconds === 0) {
+        displayNumbers.innerText = '';
+        userNumbers(randomNumbers);
+    }
+    else {
+        seconds --;
+    }
+}, 1000);
+
 
 
 
